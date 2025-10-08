b4d8df581a53eeb65ee49cd314cc0d1c5c041899
Lorenzo Ruotolo <ruotololorenzo@gmail.com>
2025-10-08 15:28:56 +0200

    bump latest carfield changes

--- run.sh
+++ run.sh
@@ -5,14 +5,18 @@
 #####################################################################
 
 GVSOC_DIR=/home/lelez/git/pulp-sdk/tools/gvsoc/
-APP_DIR="/home/lelez/git/cheshire/sw/tests/"
-#APP_DIR="/home/lelez/git/carfield/sw/tests/bare-metal/hostd/"
+#APP_DIR="/home/lelez/git/pulp-sdk/tools/gvsoc/examples/pulp-open/"
+#APP_DIR="/home/lelez/git/cheshire/sw/tests/"
+APP_DIR="/home/lelez/git/carfield/sw/tests/bare-metal/hostd/"
 VENV_DIR="./venv/"
 
-APP_NAME="dma_2d.spm.elf"
-#APP_NAME="helloworld.car.spm.elf"
-TARGET=cheshire
-GVSOC_FLAGS="--trace=insn:traces.log --trace-level=DEBUG --debug-mode"
+TRACE_FILE="traces.log"
+
+#APP_NAME="hello"
+#APP_NAME="helloworld.spm.elf"
+APP_NAME="helloworld.car.spm.elf"
+TARGET="carfield"
+GVSOC_FLAGS="--trace=:${TRACE_FILE} --trace-level=DEBUG --debug-mode" #--gdbserver
 
 # Activate the GVSoC venv
 cd $GVSOC_DIR && \
@@ -27,3 +31,7 @@ make TARGETS=$TARGET build && \
 # Run GVSoC
 echo "Running GVSoC..." && \
 gvsoc --target=$TARGET $GVSOC_FLAGS --binary $APP_DIR/$APP_NAME run
+
+# Resize log file to first 100000 lines
+head -n 100000 $TRACE_FILE > ${TRACE_FILE}.tmp && \
+mv ${TRACE_FILE}.tmp $TRACE_FILE
